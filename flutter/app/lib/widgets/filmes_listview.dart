feaef31aa5aa1fb648d5c8a44c4cbe34760880c9
Aplewicz <aplewiczcaique@gmail.com>
2026-05-05 20:30:18 -0300

    resolvendo o desafio 1

--- flutter/app/lib/widgets/filmes_listview.dart
+++ flutter/app/lib/widgets/filmes_listview.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../models/filme_item.dart';
 
 class FilmesListView extends StatelessWidget {
@@ -9,63 +8,64 @@ class FilmesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ListView.builder(
       padding: const EdgeInsets.fromLTRB(16, 16, 16, 24),
-      children: <Widget>[
-        for (final FilmeItem filme in filmes)
-          Center(
-            child: Container(
-              width: 220,
-              margin: const EdgeInsets.only(bottom: 16),
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: Colors.black12),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AspectRatio(
-                    aspectRatio: 27 / 40,
-                    child: Image.network(
-                      filme.imageUrl,
-                      fit: BoxFit.cover,
-                      errorBuilder:
-                          (
-                            BuildContext context,
-                            Object error,
-                            StackTrace? stackTrace,
-                          ) {
-                            return Container(
-                              color: const Color(0xFFB0BEC5),
-                              alignment: Alignment.center,
-                              child: const Icon(
-                                Icons.broken_image_rounded,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            );
-                          },
+      itemCount: filmes.length,
+      itemBuilder: (context, index) {
+        final filme = filmes[index];
+
+        return Center(
+          child: Container(
+            width: 220,
+            margin: const EdgeInsets.only(bottom: 16),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.black12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 27 / 40,
+                  child: Image.network(
+                    filme.imageUrl,
+                    fit: BoxFit.cover,
+                    errorBuilder: (
+                      BuildContext context,
+                      Object error,
+                      StackTrace? stackTrace,
+                    ) {
+                      return Container(
+                        color: const Color(0xFFB0BEC5),
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.broken_image_rounded,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Text(
+                    filme.titulo,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Text(
-                      filme.titulo,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-      ],
+        );
+      },
     );
   }
 }
